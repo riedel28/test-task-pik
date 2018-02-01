8a804aa8d31263a4f4ebe60b41f73e018207531e
Sergey <sergey.riedel@gmail.com>
2018-02-01 15:48:08 +0100

    Apply minor styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
-  state = {};
+  constructor() {
+    super();
+    this.state = {};
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    this.setState({});
+  }
 
   render() {
     return (
-      <form className="form">
-        <input />
+      <form className="form" onSubmit={this.handleSubmit}>
+        <h2 className="form-title">Отправить сообщение</h2>
+        <p className="form-subtitle">Анонимные сообщения рассматриваются</p>
+        <input type="text" name="name" />
+        <input type="email" name="email" />
+        <input type="text" name="birthdate" />
+        <input type="text" name="message" />
+        <button>Очистить</button>
+        <button>Отправить</button>
       </form>
     );
   }
