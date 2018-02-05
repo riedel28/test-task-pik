b1dec22aeecd012943d147c4816eef7a6b68be7f
Sergey <sergey.riedel@gmail.com>
2018-02-05 14:11:42 +0100

    Add fonts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import './css/index.css';
+import './csssrc/less/index.css';
 
 import MessageForm from './components/MessageForm';
 
